83877eff1b2b621ccb77cb141392b77ff21bf857
Takeshi Ota <singer.scottie@gmail.com>
2021-02-25 00:51:32 +0900

    [start]todo create index

--- db/migrate/20210223150327_create_tasks.rb
+++ db/migrate/20210223150327_create_tasks.rb
@@ -1,7 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-
+      
+      t.text :content
+      t.string :status
+      t.date :limit
       t.timestamps
     end
   end
