d2c93c6c891e12d80dac3af5e8403a7c056f0629
Takeshi Ota <singer.scottie@gmail.com>
2021-03-20 14:04:31 +0900

    [finish]base

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,30 +2,31 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+    if params[:id].present?
+      @task = Task.find(params[:id])
+    else
+      @task = Task.new
+    end
   end
 
   def show
     @task = Task.find(params[:id])
   end
 
-  def new
-    @task = Task.new
-  end
-
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to tasks_path
-  end
-
-  def edit
-    @task = Task.find(params[:id])
+    if @task.save
+      redirect_to tasks_path
+    else
+      @tasks = Task.all
+      render "index"
+    end
   end
 
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to tasks_path
+    redirect_to request.referer
   end
 
   def destroy
